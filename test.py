83b3c186a2c84e58a370ae919a2a8a85f22eb79b
Josh Anderson <joshand@cisco.com>
2016-11-16 21:15:55 -0600

    Added dummy test.py file

---
+++ test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+import unittest
+
+ct = 'application/json'
+
+
+class FlaskTestCase(unittest.TestCase):
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
